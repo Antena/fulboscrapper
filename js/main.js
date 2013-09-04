bf7001adb7f496c5d4b4b3554281311de03be494
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-09-04 20:52:03 -0300

    Data table

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+var app = angular.module('app', ['controllers']);
 
+var controllers = angular.module('controllers', []);
+
+controllers.controller('AppController', ['$scope', '$http', function($scope, $http) {
+    // load data
+    $http.get('data/resultados.csv')
+        .success(function(csv) {
+            $scope.games = $.csv.toObjects(csv);
+            console.log($scope.games);        //TODO(gb): Remove trace!!!
+        })
+}])
