2063b072382c6347ce3b7765417fea4c8cf62d27
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2013-09-13 14:19:09 -0300

    Minor improvements to web

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ var controllers = angular.module('controllers', []);
 controllers.controller('AppController', ['$scope', '$http', function($scope, $http) {
     // FT Client
     var ftClient = new FTClient('AIzaSyCemMVmLwJUJlriy4zSsXcQuMJNBZJQnrQ');
-    var ftTable = '1bshumjUcNd6QnB0KlT6d4856BimkgTCnlw1OgeU';
+    $scope.ftTable = '1idEJ_VNZaBtRx7VZIfcYSTk_OGJ3fBy2GKVQ6ac';
 
     // Tournaments
     $scope.tournaments = ["Apertura 90","Clausura 91","Apertura 91","Clausura 92","Apertura 92","Clausura 93","Apertura 93","Clausura 94","Apertura 94","Clausura 95","Apertura 95","Clausura 96","Apertura 96","Clausura 97","Apertura 97","Clausura 98","Apertura 98","Clausura 99","Apertura 99","Clausura 00","Apertura 00","Clausura 01","Apertura 01","Clausura 02","Apertura 02","Clausura 03","Apertura 03","Clausura 04","Apertura 04","Clausura 05","Apertura 05","Clausura 06","Apertura 06","Clausura 07","Apertura 07","Clausura 08","Apertura 08","Clausura 09","Apertura 09","Clausura 10","Apertura 10","Clausura 11","Apertura 11","Clausura 12","Inicial 12"];
@@ -13,13 +13,14 @@ controllers.controller('AppController', ['$scope', '$http', function($scope, $ht
     // Fechas
     $scope.fechas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
 
+    // Initial state
+    $scope.search = { torneo: "Apertura 90", fecha: 1 };
+
     // Update
     $scope.update = function() {
-        console.log("update");        //TODO(gb): Remove trace!!!
-        console.log($scope.search.torneo);        //TODO(gb): Remove trace!!!
         var query = {
             fields: ['date','local','visitante','resultado'],
-            table: ftTable,
+            table: $scope.ftTable,
             tail: "WHERE torneo = '" + $scope.search.torneo + "' AND fecha = '" + $scope.search.fecha + "'"
         }
 
@@ -34,10 +35,21 @@ controllers.controller('AppController', ['$scope', '$http', function($scope, $ht
                     resultado: row[3]
                 })
             })
-            console.log(games);        //TODO(gb): Remove trace!!!
             $scope.games = games;
             $scope.$apply();
         })
+    }
+
+    // Next
+    $scope.next = function() {
+        $scope.search.fecha = $scope.search.fecha < 19 ? $scope.search.fecha + 1 : $scope.search.fecha;
+        $scope.update();
+    }
+
+    // Prev
+    $scope.prev = function() {
+        $scope.search.fecha = $scope.search.fecha > 1 ? $scope.search.fecha - 1 : $scope.search.fecha;
+        $scope.update();
     }
 
     // Select tournament
@@ -45,17 +57,4 @@ controllers.controller('AppController', ['$scope', '$http', function($scope, $ht
         $scope.search.fecha = 1;
         $scope.update();
     }
-
-
-//    ftclient.query({
-//        fields: ['*'],
-//        table: '1bshumjUcNd6QnB0KlT6d4856BimkgTCnlw1OgeU'
-//    }, function(rows) {
-//        console.log(rows);        //TODO(gb): Remove trace!!!
-//    })
-//    $http.get('data/resultados.csv')
-//        .success(function(csv) {
-//            $scope.games = $.csv.toObjects(csv);
-//            console.log($scope.games);        //TODO(gb): Remove trace!!!
-//        })
 }])
